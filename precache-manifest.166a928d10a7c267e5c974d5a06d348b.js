128a2322679d79026409241d1cb8e4bd6878cc32
Gabriel <gncorreia@protonmail.com>
2022-05-03 14:10:08 -0300

    Updates

---
+++ precache-manifest.166a928d10a7c267e5c974d5a06d348b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72ec2bf696c154e2f8e2daaf92bc6835",
+    "url": "/trybewallet/index.html"
+  },
+  {
+    "revision": "e55be73433386d85853e",
+    "url": "/trybewallet/static/css/main.05f1057a.chunk.css"
+  },
+  {
+    "revision": "09a7a9143a295f25ca47",
+    "url": "/trybewallet/static/js/2.42b6b14f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/trybewallet/static/js/2.42b6b14f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e55be73433386d85853e",
+    "url": "/trybewallet/static/js/main.560a3732.chunk.js"
+  },
+  {
+    "revision": "5dc72ebdcdec490a7442",
+    "url": "/trybewallet/static/js/runtime-main.ec83dc77.js"
+  }
+]);
